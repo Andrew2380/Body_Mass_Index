6b3d3a858d805454db2141f4c207bb5865538a45
Andrew2380 <andrey238054@gmail.com>
2020-11-04 19:46:26 +0300

    Сode formatting

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public float  calculate(float weight, float  length){
-        float index =  weight / ((length/100)*(length/100));
+    public float calculate(float weight, float length) {
+        float index = weight / ((length / 100) * (length / 100));
 
         return index;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ public class Main {
 
         float weight = 80;
         float length = 180;
-        float index = service.calculate (weight, length);
+        float index = service.calculate(weight, length);
         System.out.println(index);
 
         float weight1 = 70;
         float length1 = 190;
-        float index1 = service.calculate (weight1, length1);
+        float index1 = service.calculate(weight1, length1);
         System.out.println(index1);
 
         float weight2 = 90;
         float length2 = 170;
-        float index2 = service.calculate (weight2, length2);
+        float index2 = service.calculate(weight2, length2);
         System.out.println(index2);
 
 
